fbfa02f204b4c605a3fb670cdd05bbbcfe0246fb
Iván <ivan.mansillacor@gmail.com>
2025-12-21 17:31:27 -0300

    feat: heredoc

--- src/builtin.c
+++ src/builtin.c
@@ -54,16 +54,44 @@ void reOut(char** args){
   }
 }
 
+
 void reIn(char** args){
   int i=0;
   while(args[i] != NULL){
-    if(strcmp(args[i], "<") == 0){
+    if(strcmp(args[i], "<") == 0 || strcmp(args[i], "<<") == 0){
       if(args[i+1] == NULL){
         printf("ERROR: No file to read\n");
         exit(EXIT_FAILURE);
       }
+      
+      char* file_name;
 
-      char* file_name = args[i+1];
+      if(strcmp(args[i],"<<") == 0){
+        char* stop_word = args[i+1];
+        file_name = ".tmp";
+
+        int temp_fd = open(file_name, O_CREAT | O_WRONLY | O_TRUNC,0644);
+
+        char* line = NULL;
+        size_t len = 0;
+        ssize_t read;
+
+        while(1){
+          printf("heredoc> ");
+          read = getline(&line, &len, stdin);
+          
+          if(strncmp(line, stop_word, strlen(stop_word)) == 0 && (read - 1) == strlen(stop_word)){
+            break;
+          }
+
+          write(temp_fd,line,read);
+        }
+        free(line);
+        close(temp_fd);
+      }
+      else if(strcmp(args[i],"<") == 0){
+        file_name = args[i+1];
+      }
 
       int fd = open(file_name, O_RDONLY);
       if(fd == -1){
@@ -73,6 +101,10 @@ void reIn(char** args){
 
       dup2(fd, STDIN_FILENO);
       close(fd);
+
+      if(strcmp(args[i], "<<") == 0){
+        unlink(file_name);
+      }
 
       args[i] = NULL;
     }
